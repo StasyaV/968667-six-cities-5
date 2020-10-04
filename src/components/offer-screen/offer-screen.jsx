fa81f3dfe8b87dcfc67eedefcbe0dd19a5be3523
StasyaV <stasya522@mail.ru>
2020-10-04 18:22:50 +0300

    Правки в ссылках

--- src/components/offer-screen/offer-screen.jsx
+++ src/components/offer-screen/offer-screen.jsx
@@ -7,22 +7,22 @@ const OfferScreen = () => {
         <div className="property__gallery-container container">
           <div className="property__gallery">
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/room.jpg" alt="Photo studio"/>
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/apartment-02.jpg" alt="Photo studio"/>
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/apartment-03.jpg" alt="Photo studio"/>
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/studio-01.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/studio-01.jpg" alt="Photo studio"/>
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+              <img className="property__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
             </div>
           </div>
         </div>
@@ -103,7 +103,7 @@ const OfferScreen = () => {
               <h2 className="property__host-title">Meet the host</h2>
               <div className="property__host-user user">
                 <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                  <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
+                  <img className="property__avatar user__avatar" src="/img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
                 </div>
                 <span className="property__user-name">
                     Angelina
@@ -124,7 +124,7 @@ const OfferScreen = () => {
                 <li className="reviews__item">
                   <div className="reviews__user user">
                     <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                      <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
+                      <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
                     </div>
                     <span className="reviews__user-name">
                         Max
@@ -202,7 +202,7 @@ const OfferScreen = () => {
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                  <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
                 </a>
               </div>
               <div className="place-card__info">
@@ -234,7 +234,7 @@ const OfferScreen = () => {
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image"/>
+                  <img className="place-card__image" src="/img/apartment-02.jpg" width="260" height="200" alt="Place image"/>
                 </a>
               </div>
               <div className="place-card__info">
@@ -266,7 +266,7 @@ const OfferScreen = () => {
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="#">
-                  <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
+                  <img className="place-card__image" src="/img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
                 </a>
               </div>
               <div className="place-card__info">
