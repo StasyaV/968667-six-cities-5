4521423d591e4a6a39222e7d0969823dcfed5d4d
StasyaV <stasya522@mail.ru>
2020-10-22 22:31:56 +0300

    Добавлен hoc для формы

---
+++ src/hocs/with-form.jsx
@@ -0,0 +1,55 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+
+const withForm = (Component) => {
+  return class WithForm extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        id: null,
+        author: `Max`,
+        avatar: `img/avatar-max.jpg`,
+        rating: null,
+        text: null,
+        date: new Date(),
+      };
+
+      this._handleRatingChange = this._handleRatingChange.bind(this);
+      this._handleTextFieldChange = this._handleTextFieldChange.bind(this);
+      this._handleSubmit = this._handleSubmit.bind(this);
+    }
+
+    _handleSubmit(evt) {
+      evt.preventDefault();
+    }
+
+    _handleRatingChange(evt) {
+      this.setState({
+        rate: evt.target.value,
+      });
+    }
+
+    _handleTextFieldChange(evt) {
+      this.setState({
+        comment: evt.target.value,
+      });
+    }
+
+    render() {
+      return <Component
+        {...this.props}
+        onSubmit={this._handleSubmit}
+        onRatingChange={this._handleRatingChange}
+        onTextFieldChange={this._handleTextFieldChange}
+      />;
+    }
+  };
+};
+
+
+withForm.propTypes = {
+  comments: PropTypes.array.isRequired,
+};
+
+export default withForm;
