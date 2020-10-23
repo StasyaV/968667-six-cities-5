b21f7fb45c31d2a8174639b757870eb07861676d
StasyaV <stasya522@mail.ru>
2020-10-23 17:27:52 +0300

    Убрала лишний хок

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import {getRating} from "../../utils/utils";
-import {withActiveFlag} from "../../hocs/with-active-flag";
 
 const OfferCard = (props) => {
-  const {offer, onActiveChange} = props;
+  const {offer} = props;
 
   return (
     <article className="cities__place-card place-card" id={offer.id}
@@ -27,7 +26,7 @@ const OfferCard = (props) => {
             <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button onClick={onActiveChange} className={`place-card__bookmark-button button ${offer.isFavorite ? `place-card__bookmark-button--active` : ``}`} type="button">
+          <button className={`place-card__bookmark-button button ${offer.isFavorite ? `place-card__bookmark-button--active` : ``}`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -62,7 +61,6 @@ OfferCard.propTypes = {
     isPremium: PropTypes.bool.isRequired,
     comments: PropTypes.array,
   }).isRequired,
-  onActiveChange: PropTypes.func.isRequired,
 };
 
-export default withActiveFlag(OfferCard);
+export default OfferCard;
