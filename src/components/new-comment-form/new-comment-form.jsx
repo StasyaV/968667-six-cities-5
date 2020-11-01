4620f66ffccc0f93dec631ca1ed86e9d44abf8e7
StasyaV <stasya522@mail.ru>
2020-11-01 22:20:56 +0300

    Отправка коммента

--- src/components/new-comment-form/new-comment-form.jsx
+++ src/components/new-comment-form/new-comment-form.jsx
@@ -1,46 +1,37 @@
-import React, {PureComponent, createRef} from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import withForm from "../../hocs/with-form";
+import {CommentLength} from "../../const";
 import {connect} from "react-redux";
 import {sendComment} from "../../store/api-actions";
 
 class NewCommentForm extends PureComponent {
   constructor(props) {
     super(props);
-
-    this.textRef = createRef();
-    this.ratingRef = createRef();
-    this.buttonRef = createRef();
-
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
   }
 
-  handleSubmit(evt) {
-    const {onCommentSubmit} = this.props;
-
+  _handleFormSubmit(evt) {
+    const {onCommentSubmit, rating, comment, offerId, resetState} = this.props;
     evt.preventDefault();
-    const offerId = evt.target.closest(`.property`).id;
-
-    if (!this.textRef || !this.ratingRef || this.textRef.value > 300 || this.textRef.value < 50) {
-      this.buttonRef.disabled = true;
-      this.textRef.style.border = `red`;
-    }
 
     onCommentSubmit({
-      comment: this.textRef.current.value,
-      rating: this.ratingRef.current.value,
+      comment,
+      rating
     }, offerId);
 
-    this.textRef.current.value = ``;
-    this.ratingRef.current.value = ``;
+    resetState(evt);
   }
-
   render() {
+    const {rating, comment, onRatingChange, onCommentInputChange} = this.props;
+    console.log(comment.length, `comment`);
+    console.log(rating, `rating`);
     return (
-      <form onSubmit={this.handleSubmit} className="reviews__form form" action="#" method="post">
+      <form onSubmit={this._handleFormSubmit} className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
           <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-            ref={this.ratingRef}/>
+            onChange={onRatingChange}/>
           <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -48,7 +39,7 @@ class NewCommentForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-            ref={this.ratingRef}/>
+            onChange={onRatingChange}/>
           <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -56,7 +47,7 @@ class NewCommentForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
-            ref={this.ratingRef}/>
+            onChange={onRatingChange}/>
           <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -64,7 +55,7 @@ class NewCommentForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
-            ref={this.ratingRef}/>
+            onChange={onRatingChange}/>
           <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -72,7 +63,7 @@ class NewCommentForm extends PureComponent {
           </label>
 
           <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
-            ref={this.ratingRef}/>
+            onChange={onRatingChange}/>
           <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
@@ -80,12 +71,15 @@ class NewCommentForm extends PureComponent {
           </label>
         </div>
         <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"
-          ref={this.textRef}></textarea>
+          onChange={onCommentInputChange}></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
           </p>
-          <button ref={this.buttonRef} className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+          <button
+            className="reviews__submit form__submit button" type="submit"
+            disabled={comment.length < CommentLength.MIN || comment.length >= CommentLength.MAX || !rating || !comment ? true : false}>
+            Submit</button>
         </div>
       </form>
     );
@@ -93,14 +87,24 @@ class NewCommentForm extends PureComponent {
 }
 
 NewCommentForm.propTypes = {
+  rating: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
   onCommentSubmit: PropTypes.func.isRequired,
+  onCommentInputChange: PropTypes.func.isRequired,
+  onRatingChange: PropTypes.func.isRequired,
+  offerId: PropTypes.string.isRequired,
+  resetState: PropTypes.func.isRequired
 };
 
-const mapDispatchToProps = (dispatch) => ({
+const mapStateToProps = ({ACTIONS}) => ({
+  offerId: ACTIONS.activeOfferId
+});
+
+const mapDispatchToProps = ((dispatch) => ({
   onCommentSubmit(commentData, offerId) {
     dispatch(sendComment(commentData, offerId));
   }
-});
+}));
 
-export {NewCommentForm};
-export default connect(null, mapDispatchToProps)(NewCommentForm);
+export const form = withForm(NewCommentForm);
+export default connect(mapStateToProps, mapDispatchToProps)(withForm(NewCommentForm));
--- src/hocs/with-form.jsx
+++ src/hocs/with-form.jsx
@@ -1,5 +1,4 @@
 import React, {PureComponent} from "react";
-import PropTypes from "prop-types";
 
 const withForm = (Component) => {
   return class WithForm extends PureComponent {
@@ -7,17 +6,13 @@ const withForm = (Component) => {
       super(props);
 
       this.state = {
-        rating: null,
-        text: null
+        rating: `0`,
+        comment: ``
       };
-      
+
       this._handleRatingChange = this._handleRatingChange.bind(this);
       this._handleTextFieldChange = this._handleTextFieldChange.bind(this);
-      this._handleSubmit = this._handleSubmit.bind(this);
-    }
-
-    _handleSubmit(evt) {
-      evt.preventDefault();
+      this._resetState = this._resetState.bind(this);
     }
 
     _handleRatingChange(evt) {
@@ -32,20 +27,25 @@ const withForm = (Component) => {
       });
     }
 
+    _resetState(evt) {
+      this.setState({
+        rating: evt.target.value = ``,
+        comment: evt.target.value = ``
+      });
+    }
+
     render() {
+      const {rating, comment} = this.state;
       return <Component
         {...this.props}
-        onSubmit={this._handleSubmit}
         onRatingChange={this._handleRatingChange}
-        onTextFieldChange={this._handleTextFieldChange}
+        onCommentInputChange = {this._handleTextFieldChange}
+        rating={rating}
+        comment={comment}
+        resetState={this._resetState}
       />;
     }
   };
-};
-
-
-withForm.propTypes = {
-  comments: PropTypes.array.isRequired,
 };
 
 export default withForm;
